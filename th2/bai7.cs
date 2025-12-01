d4cf12793da0abf249b784f0080458889c669dff
Nguyen Tinh <nguyenvantinh170505@gmail.com>
2025-12-01 16:52:13 +0700

    Update bai7.cs

--- th2/bai7.cs
+++ th2/bai7.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace BT7
+namespace WDFB2
 {
-    public partial class Form1 : Form
+    public partial class Bai7 : Form
     {
-        public Form1()
+        public Bai7()
         {
             InitializeComponent();
         }
@@ -99,3 +99,4 @@ namespace BT7
         }
     }
 }
+
